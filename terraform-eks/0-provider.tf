3e4760c488c667b48fab450ce164e6b4a9b8eaff
jin <djzepssa@gmail.com>
2023-12-20 17:31:33 +0900

    update

--- terraform-eks/0-provider.tf
+++ terraform-eks/0-provider.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 terraform {
   required_providers {
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
-    }
+    # kubectl = {
+    #   source  = "gavinbunney/kubectl"
+    #   version = ">= 1.14.0"
+    # }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.6.0"
@@ -16,6 +16,11 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.0"
   }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
 }
   required_version = "~> 1.0"
 }
